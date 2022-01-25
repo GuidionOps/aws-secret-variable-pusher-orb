e599bf70268a54d53715cd0b740d6cb6d7cb6e60
Eagleman <eagleman7@quicknet.nl>
2022-01-25 11:42:35 +0100

    feat: Loop through PARAM_CIRCLECI_VARIABLES

--- src/scripts/aws-secret-variable-pusher.sh
+++ src/scripts/aws-secret-variable-pusher.sh
@@ -1,15 +1,21 @@
 #!/bin/bash	
 
-if [[ $(aws secretsmanager describe-secret --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_CIRCLECI_VARIABLE}" 2>&1 || true) =~ "ResourceNotFoundException" ]]; then
+for i in ${PARAM_CIRCLECI_VARIABLE//,/ }
+do
+if [[ $(aws secretsmanager describe-secret --secret-id "${CIRCLE_PROJECT_REPONAME}-${i}" 2>&1 || true) =~ "ResourceNotFoundException" ]]; then
     echo "AWS secret not found"
-    aws secretsmanager create-secret --name "${CIRCLE_PROJECT_REPONAME}-${PARAM_CIRCLECI_VARIABLE}" --secret-string $"${!PARAM_CIRCLECI_VARIABLE}" --kms-key-id "${PARAM_AWS_KMS_KEY}"
+    aws secretsmanager create-secret --name "${CIRCLE_PROJECT_REPONAME}-${i}" --secret-string $"${!i}" --kms-key-id "${PARAM_AWS_KMS_KEY}"
     exit 0
 fi
+done
 
-if [[ "$(aws secretsmanager get-secret-value --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_CIRCLECI_VARIABLE}" | jq --raw-output .SecretString)" == $"${!PARAM_CIRCLECI_VARIABLE}" ]]; then
+for i in ${PARAM_CIRCLECI_VARIABLE//,/ }
+do
+if [[ "$(aws secretsmanager get-secret-value --secret-id "${CIRCLE_PROJECT_REPONAME}-${i}" | jq --raw-output .SecretString)" == $"${!i}" ]]; then
     echo "Variable is still the same on AWS"
     exit 0
 else
     echo "Variable changed, updating on AWS"
-    aws secretsmanager update-secret --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_CIRCLECI_VARIABLE}" --secret-string $"${!PARAM_CIRCLECI_VARIABLE}" --kms-key-id "${PARAM_AWS_KMS_KEY}"
+    aws secretsmanager update-secret --secret-id "${CIRCLE_PROJECT_REPONAME}-${i}" --secret-string $"${!i}" --kms-key-id "${PARAM_AWS_KMS_KEY}"
 fi
+done
