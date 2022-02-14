a50fc5c3f4563adf8453293aeebfd601fe3e7c37
Eagleman <eagleman7@quicknet.nl>
2022-02-14 11:27:48 +0100

    feat: Use / instead of - for secret-names

--- src/scripts/aws-secret-variable-pusher.sh
+++ src/scripts/aws-secret-variable-pusher.sh
@@ -2,14 +2,14 @@
 
 for i in ${PARAM_CIRCLECI_VARIABLE//,/ }
 do
-if [[ $(aws secretsmanager describe-secret --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" 2>&1 || true) =~ "ResourceNotFoundException" ]]; then
+if [[ $(aws secretsmanager describe-secret --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" 2>&1 || true) =~ "ResourceNotFoundException" ]]; then
     echo "AWS secret not found"
-    aws secretsmanager create-secret --name "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --secret-string $"${!i}" --kms-key-id "${PARAM_AWS_KMS_KEY}"
+    aws secretsmanager create-secret --name "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --secret-string $"${!i}" --kms-key-id "${PARAM_AWS_KMS_KEY}"
 
-    aws secretsmanager tag-resource --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --tags Key=Enviroment,Value="${AWS_ACCOUNT_NAME}"
-    aws secretsmanager tag-resource --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --tags Key=Owner,Value="${CIRCLE_PROJECT_USERNAME}"
-    aws secretsmanager tag-resource --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --tags Key=GitRepository,Value="${CIRCLE_PROJECT_REPONAME}"
-    aws secretsmanager tag-resource --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --tags Key=DeploymentTool,Value="CircleCI"
+    aws secretsmanager tag-resource --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --tags Key=Enviroment,Value="${AWS_ACCOUNT_NAME}"
+    aws secretsmanager tag-resource --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --tags Key=Owner,Value="${CIRCLE_PROJECT_USERNAME}"
+    aws secretsmanager tag-resource --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --tags Key=GitRepository,Value="${CIRCLE_PROJECT_REPONAME}"
+    aws secretsmanager tag-resource --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --tags Key=DeploymentTool,Value="CircleCI"
 
     continue
 fi
@@ -17,17 +17,17 @@ done
 
 for i in ${PARAM_CIRCLECI_VARIABLE//,/ }
 do
-if [[ "$(aws secretsmanager get-secret-value --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" | jq --raw-output .SecretString)" == $"${!i}" ]]; then
+if [[ "$(aws secretsmanager get-secret-value --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" | jq --raw-output .SecretString)" == $"${!i}" ]]; then
     echo "Variable is still the same on AWS"
     continue
 else
     echo "Variable changed, updating on AWS"
-    aws secretsmanager update-secret --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --secret-string $"${!i}" --kms-key-id "${PARAM_AWS_KMS_KEY}"
+    aws secretsmanager update-secret --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --secret-string $"${!i}" --kms-key-id "${PARAM_AWS_KMS_KEY}"
 
-    aws secretsmanager tag-resource --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --tags Key=Enviroment,Value="${AWS_ACCOUNT_NAME}"
-    aws secretsmanager tag-resource --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --tags Key=Owner,Value="${CIRCLE_PROJECT_USERNAME}"
-    aws secretsmanager tag-resource --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --tags Key=GitRepository,Value="${CIRCLE_PROJECT_REPONAME}"
-    aws secretsmanager tag-resource --secret-id "${CIRCLE_PROJECT_REPONAME}-${PARAM_AWS_ENVIROMENT}-${i}" --tags Key=DeploymentTool,Value="CircleCI"
+    aws secretsmanager tag-resource --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --tags Key=Enviroment,Value="${AWS_ACCOUNT_NAME}"
+    aws secretsmanager tag-resource --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --tags Key=Owner,Value="${CIRCLE_PROJECT_USERNAME}"
+    aws secretsmanager tag-resource --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --tags Key=GitRepository,Value="${CIRCLE_PROJECT_REPONAME}"
+    aws secretsmanager tag-resource --secret-id "/${CIRCLE_PROJECT_REPONAME}/${PARAM_AWS_ENVIROMENT}/${i}" --tags Key=DeploymentTool,Value="CircleCI"
 
 fi
 done
